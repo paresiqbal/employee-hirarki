31bb67f2de93c5bf7680023aaaab2e743a045bcb
paresiqbal <thecrowdel@gmail.com>
2023-07-04 19:36:04 +0700

    Update up to task 3

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-function generateEmployeeHierarchies(data) {
-  const { projectName, employeeHierarchiesToDisplay } = data;
+function generateEmployeeHierarchies(projects) {
+  const output = [];
 
-  const employeeHierarchies = employeeHierarchiesToDisplay.map(
-    (employeeName) => {
-      const superiors = findSuperiors(employeeName, data.employeeHierarchies);
-      return { employee: employeeName, superiors };
+  projects.forEach((project) => {
+    const {
+      projectName,
+      employees,
+      employeeHierarchies,
+      employeeHierarchiesToDisplay,
+    } = project;
+    const employeeHierarchiesResult = [];
+
+    employeeHierarchiesToDisplay.forEach((employeeName) => {
+      const superiors = findSuperiors(employeeName, employeeHierarchies);
+      if (superiors) {
+        employeeHierarchiesResult.push({ employee: employeeName, superiors });
+      }
+    });
+
+    const error = checkForErrors(
+      employees,
+      employeeHierarchies,
+      employeeHierarchiesResult
+    );
+
+    if (error) {
+      output.push({ projectName, error });
+    } else {
+      output.push({
+        projectName,
+        employeeHierarchies: employeeHierarchiesResult,
+      });
     }
-  );
+  });
 
-  return { projectName, employeeHierarchies };
+  return output;
 }
 
 function findSuperiors(employeeName, employeeHierarchies) {
@@ -24,44 +49,121 @@ function findSuperiors(employeeName, employeeHierarchies) {
   }
 
   searchForSuperiors(employeeName);
-  return superiors;
+  return superiors.length ? superiors : null;
 }
 
-// Example usage
-const data = {
-  projectName: "ProjectMidland",
-  employees: [
-    "raelynn",
-    "darin",
-    "kacie",
-    "jordana",
-    "everett",
-    "bertha",
-    "peg",
-    "hugh",
-    "eveleen",
-    "evelina",
-  ],
-  employeeHierarchies: [
-    {
-      employeeName: "raelynn",
-      directReports: ["darin", "kacie"],
-    },
-    {
-      employeeName: "darin",
-      directReports: ["jordana", "everett", "bertha"],
-    },
-    {
-      employeeName: "kacie",
-      directReports: ["peg", "hugh", "eveleen"],
-    },
-    {
-      employeeName: "eveleen",
-      directReports: ["evelina"],
-    },
-  ],
-  employeeHierarchiesToDisplay: ["evelina", "bertha"],
-};
+function checkForErrors(
+  employees,
+  employeeHierarchies,
+  employeeHierarchiesResult
+) {
+  const employeeSet = new Set(employees);
+  const employeesWithoutHierarchy = new Set(employees);
 
-const output = generateEmployeeHierarchies(data);
+  employeeHierarchies.forEach((hierarchy) => {
+    const { employeeName, directReports } = hierarchy;
+    employeesWithoutHierarchy.delete(employeeName);
+    directReports.forEach((report) => employeesWithoutHierarchy.delete(report));
+  });
+
+  if (employeesWithoutHierarchy.size > 0) {
+    const error = `unable to process employee tree. ${[
+      ...employeesWithoutHierarchy,
+    ].join(",")} not having hierarchy`;
+    return error;
+  }
+
+  const employeeManagerCounts = {};
+
+  employeeHierarchies.forEach((hierarchy) => {
+    const { employeeName, directReports } = hierarchy;
+    directReports.forEach((report) => {
+      if (employeeManagerCounts[report]) {
+        employeeManagerCounts[report].push(employeeName);
+      } else {
+        employeeManagerCounts[report] = [employeeName];
+      }
+    });
+  });
+
+  const invalidManagers = Object.entries(employeeManagerCounts).filter(
+    ([_, managers]) => managers.length > 1
+  );
+
+  if (invalidManagers.length > 0) {
+    const error = `unable to process employee tree. ${invalidManagers
+      .map(([employee]) => employee)
+      .join(",")} has multiple managers: ${invalidManagers
+      .map(([, managers]) => managers.join(","))
+      .join(";")}`;
+    return error;
+  }
+
+  return null;
+}
+
+const projects = [
+  {
+    projectName: "ProjectMidland",
+    employees: [
+      "raelynn",
+      "darin",
+      "kacie",
+      "jordana",
+      "everett",
+      "bertha",
+      "peg",
+      "hugh",
+      "eveleen",
+      "evelina",
+    ],
+    employeeHierarchies: [
+      {
+        employeeName: "raelynn",
+        directReports: ["darin", "kacie"],
+      },
+      {
+        employeeName: "darin",
+        directReports: ["jordana", "everett", "bertha"],
+      },
+      {
+        employeeName: "kacie",
+        directReports: ["peg", "hugh", "eveleen"],
+      },
+      {
+        employeeName: "eveleen",
+        directReports: ["evelina"],
+      },
+    ],
+    employeeHierarchiesToDisplay: ["evelina", "bertha"],
+  },
+  {
+    projectName: "ProjectQuant",
+    employees: ["maurice", "hayleigh", "edwyn", "keane", "kylee"],
+    employeeHierarchies: [
+      {
+        employeeName: "maurice",
+        directReports: ["hayleigh", "edwyn"],
+      },
+      {
+        employeeName: "hayleigh",
+        directReports: ["edwyn", "keane", "kylee"],
+      },
+    ],
+    employeeHierarchiesToDisplay: ["kylee", "edwyn"],
+  },
+  {
+    projectName: "ProjectGeorgia",
+    employees: ["lori", "fletcher", "tabitha", "linton", "tressa", "rosanna"],
+    employeeHierarchies: [
+      {
+        employeeName: "tabitha",
+        directReports: ["linton", "tressa"],
+      },
+    ],
+    employeeHierarchiesToDisplay: ["rosanna"],
+  },
+];
+
+const output = generateEmployeeHierarchies(projects);
 console.log(JSON.stringify(output, null, 2));
